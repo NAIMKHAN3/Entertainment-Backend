c2ad3bb1f36e55f6b989ffd46180d23802a9c17f
NAIMKHAN3 <naimkhan01856@gmail.com>
2023-10-14 01:57:49 +0600

    Blog model create

--- src/rating/rating.controller.ts
+++ src/rating/rating.controller.ts
@@ -17,19 +17,22 @@ export const createRating = async (req: Request, res: Response, next: NextFuncti
         next(err)
     }
 }
-// export const createRating = async (req: Request, res: Response, next: NextFunction) => {
-//     try {
-//         const { id } = req.user;
-//         req.body.userId = id;
-//         const result = await prisma.cenemaRating.create({ data: req.body })
-//         res.status(201).send({
-//             success: true,
-//             statusCode: 201,
-//             message: 'Create Rating Successfully',
-//             data: result
-//         })
-//     }
-//     catch (err) {
-//         next(err)
-//     }
-// }
+export const getRating = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const result = await prisma.cenemaRating.findMany({
+             include:{
+                user:true,
+                 cenema:true
+                }
+            })
+        res.status(200).send({
+            success: true,
+            statusCode: 200,
+            message: 'Get Rating Successfully',
+            data: result
+        })
+    }
+    catch (err) {
+        next(err)
+    }
+}
